c47df6b572c92a4a4a2528d18da7262cbeea65a2
ROVYYY123 <Arevalo.rovy@gmail.com>
2024-11-14 11:18:21 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Apps from "./components/Apps";
+import About from "./components/About";
+import Learning from "./components/Learning";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (  
+    <div >
+      <Navbar/>
+      <Hero/>
+      <Apps/>
+      <About/>
+      <Learning/>
+      <Work/>
+      <Contact/>
+     
     </div>
   );
 }
